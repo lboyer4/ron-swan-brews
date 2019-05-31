f8d348de8c07ac1e41f7e427570f017aaeb6cf9f
Lauren Boyer <lboyer4@outlook.com>
2019-05-31 11:32:12 -0600

    Added search dispatch to reset to clear

--- src/containers/Icon/index.js
+++ src/containers/Icon/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './_Icon.scss';
 import ronswanIcon from '../../utils/images/ronswanIcon.png'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setSearch } from '../../actions';
 
 class Icon extends Component {
 	constructor() {
@@ -11,19 +13,36 @@ class Icon extends Component {
 		}
 	}
 
+	clearSearch = () => {
+		const clear = ''
+		this.props.setSearch(clear)
+	}
+
 	render() {
 		return (
 			<div className="icon-holder">
 				<nav>
-					<Link to='/'>
+					<Link to='/' onClick={this.clearSearch}>
 					<img src={ronswanIcon} height="100px" alt="icon with an outline of ron swanson's face and hair" />
 					</Link>
-					<h1 className="title">RonSwanBreweries</h1>
+					<div className="title">
+					<h1>RonSwanBreweries</h1>
+				
+				<h6 className="ronswan-quote">"Clear alcohols are for rich women on diets."</h6>
+				<p> -Ron Swanson</p>
+				</div>
 				</nav>
-				<p className="ronswan-quote">quote will eventually go here and wont that be nice</p>
 			</div>
 			)
 	}
 }
 
-export default Icon;
+export const mapStateToProps = (state) => ({
+	quotes: state.quotes
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+	setSearch: (search) => dispatch(setSearch(search))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Icon)

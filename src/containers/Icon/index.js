517f66913557810ffd7afec9ca76257cb6a44007
Lauren Boyer <lboyer4@outlook.com>
2019-05-31 11:09:16 -0600

    icon takes you back to the home slash route

--- src/containers/Icon/index.js
+++ src/containers/Icon/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './_Icon.scss';
 import ronswanIcon from '../../utils/images/ronswanIcon.png'
+import { Link } from 'react-router-dom';
 
 class Icon extends Component {
 	constructor() {
@@ -14,7 +15,9 @@ class Icon extends Component {
 		return (
 			<div className="icon-holder">
 				<nav>
+					<Link to='/'>
 					<img src={ronswanIcon} height="100px" alt="icon with an outline of ron swanson's face and hair" />
+					</Link>
 					<h1 className="title">RonSwanBreweries</h1>
 				</nav>
 				<p className="ronswan-quote">quote will eventually go here and wont that be nice</p>

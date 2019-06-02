f7dd11436e36af3e28e44e134e5370f8c419d4ab
Lauren Boyer <lboyer4@outlook.com>
2019-06-02 09:14:42 -0600

    tested toggleFavorites mdtp in brewerydetails

--- src/containers/BreweryDetails/BreweryDetails.test.js
+++ src/containers/BreweryDetails/BreweryDetails.test.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { BreweryDetails } from '../BreweryDetails';
+import { BreweryDetails, mapDispatchToProps } from '../BreweryDetails';
+import { toggleFavorite } from '../../actions';
 
 describe('BreweryDetails', () => {
 	let wrapper;
+	let mockToggleFavorite;
 
 	beforeEach(() => {
+		mockToggleFavorite = jest.fn()
+
 		wrapper = shallow(
 			<BreweryDetails
+				toggleFavorite={mockToggleFavorite}
 			/>
 		);
 	});
 
 	it('should match snapshot', () => {
 		expect(wrapper).toMatchSnapshot()
+	});
+
+	it('should call dispatch when using a function from MDTP', () => {
+		const mockId = 1
+		const mockDispatch = jest.fn();
+		const actionToDispatch = toggleFavorite(mockId);
+		const mappedProps = mapDispatchToProps(mockDispatch);
+
+		mappedProps.toggleFavorite(1)
+
+		expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
 	});
 });

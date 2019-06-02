5a5bd8382865cba210b6a3e6e36faa882f8ccf9c
Lauren Boyer <lboyer4@outlook.com>
2019-06-02 09:27:58 -0600

    tested mstp items in breweryHolder

--- src/containers/breweryHolder/BreweryHolder.test.js
+++ src/containers/breweryHolder/BreweryHolder.test.js
@@ -7,9 +7,10 @@ describe('BreweryHolder', () => {
 	let wrapper;
 	let mockBreweries;
 	let mockQuotes;
+	let mockSearch;
 
 	beforeEach(() => {
-		let mockBreweries = [ 
+		mockBreweries = [ 
 			{
 				brewery: 'brew1'
 			},
@@ -18,20 +19,61 @@ describe('BreweryHolder', () => {
 			}
 		]
 
-		let mockQuotes = [
+		mockQuotes = [
 			'quote one',
 			'quote two'
 		]
+
+		mockSearch = 'Denver';
 
 		wrapper = shallow(
 			<BreweryHolder
 				breweries = {mockBreweries}
 				quotes = {mockQuotes}
+				search = {mockSearch}
 			/>
 		)
 	})
 
 	it('should match snapshots', () => {
 		expect(wrapper).toMatchSnapshot();
-	})
-})
+	});
+
+	describe('mapStateToProps', () => {
+		it('should return a breweries object', () => {
+			const mockState = {
+				breweries: { breweries: mockBreweries }
+			}
+			const expected = {
+				breweries: mockState.breweries
+			}
+			const mappedProps = mapStateToProps(mockState);
+
+			expect(mappedProps).toEqual(expected)
+		});
+
+		it('should return a quotes object', () => {
+			const mockState = {
+				quotes: { quotes: mockQuotes }
+			}
+			const expected = {
+				quotes: mockState.quotes
+			}
+			const mappedProps = mapStateToProps(mockState);
+
+			expect(mappedProps).toEqual(expected);
+		});
+
+		it('should return a search object', () => {
+			const mockState = {
+				search: { search: mockSearch}
+			}
+			const expected = {
+				search: mockState.search
+			}
+			const mappedProps = mapStateToProps(mockState);
+
+			expect(mappedProps).toEqual(expected);
+		});
+	});
+});

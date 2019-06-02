ec31707128d254e9037726e68042d8346efddc21
Lauren Boyer <lboyer4@outlook.com>
2019-06-02 08:13:10 -0600

    Added PropTypes to BreweryHolder

--- src/containers/breweryHolder/index.js
+++ src/containers/breweryHolder/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Card from '../Card';
-import { connect } from 'react-redux'
-import './_BreweryHolder.scss'
+import { connect } from 'react-redux';
+import './_BreweryHolder.scss';
+import PropTypes from 'prop-types';
 
 export const BreweryHolder = (props) => {
 	let allCards = props.breweries.map(brewery => {
 			return <Card {...brewery} key={brewery.id}/>
 		})
-	let showCard = props.search === 'Show All' && allCards
+	let showCard = props.search === 'Show All' && allCards;
 	
 	let min = Math.ceil(1);
   let max = Math.floor(30);
   let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-	let quote = props.quotes[randomNum]
+	let quote = props.quotes[randomNum];
 
 	return (
 		<section className="card-holder">
@@ -20,12 +21,22 @@ export const BreweryHolder = (props) => {
 				{showCard}
 		</section>
 	)
-}
+};
 
 export const mapStateToProps = (state) => ({
 	breweries: state.breweries,
 	quotes: state.quotes,
 	search: state.search
-})
+});
 
 export default connect(mapStateToProps)(BreweryHolder);
+
+BreweryHolder.propTypes = {
+	breweries: PropTypes.array,
+	dispatch: PropTypes.func,
+	history: PropTypes.object,
+	location: PropTypes.object,
+	match:PropTypes.object,
+	quotes:PropTypes.array,
+	search: PropTypes.string
+}
